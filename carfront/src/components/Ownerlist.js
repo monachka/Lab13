e227e8031aaae44ad25cdb6da408d277d5152b71
monachka <142250148+monachka@users.noreply.github.com>
2023-10-24 07:02:54 +0000

    Add files via upload

--- carfront/src/components/Ownerlist.js
+++ carfront/src/components/Ownerlist.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
-//import Snackbar from "@mui/material/Snackbar";
+import Snackbar from "@mui/material/Snackbar";
 import { SERVER_URL } from "../constants.js";
 import { DataGrid } from "@mui/x-data-grid";
-//import Button from "@mui/material/Button";
-// import AddCar from "./AddCar.js";
-// import EditCar from './EditCar.js';
-// import Stack from '@mui/material/Stack';
-// import IconButton from '@mui/material/IconButton';
-// import DeleteIcon from '@mui/icons-material/Delete';
+import Button from "@mui/material/Button";
+import AddOwner from "./AddOwner.js";
+import EditOwner from './EditOwner.js';
+import Stack from '@mui/material/Stack';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
 //import logo from '../logo.svg';
 
 function OwnerList() {
@@ -24,74 +24,74 @@ function OwnerList() {
       .catch((err) => console.error(err));
   };
 
-//   const addCar = car => {
-//     fetch(SERVER_URL + "api/cars", {
-//       method: "POST",
-//       headers: { "Content-Type": "application/json" },
-//       body: JSON.stringify(car),
-//     }).then((response) => {
-//       if (response.ok) {
-//         fetchCars();
-//       } else {
-//         alert("Something went wrong !!");
-//       }
-//     }).catch(err => console.error(err));
-//   };
+  const addOwner = owner => {
+    fetch(SERVER_URL + "api/owners", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(owner),
+    }).then((response) => {
+      if (response.ok) {
+        fetchOwners();
+      } else {
+        alert("Something went wrong !!");
+      }
+    }).catch(err => console.error(err));
+  };
 
-//   const updateCar = (car, link) => {
-//     fetch(link, {
-//       method: "PUT",
-//       headers: { "Content-Type": "application/json" },
-//       body: JSON.stringify(car),
-//     }).then((response) => {
-//       if (response.ok) {
-//         fetchCars();
-//       } else {
-//         alert("Something went wrong !!");
-//       }
-//     }).catch(err => console.error(err));
-//   };
+  const updateOwner = (owner, link) => {
+    fetch(link, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(owner),
+    }).then((response) => {
+      if (response.ok) {
+        fetchOwners();
+      } else {
+        alert("Something went wrong !!");
+      }
+    }).catch(err => console.error(err));
+  };
 
-//   const onDelClick = url => {
-//     if (window.confirm("Are you sure to delete ?")) {
-//       fetch(url, { method: "DELETE" })
-//         .then((response) => {
-//           if (response.ok) {
-//             fetchCars();
-//             setOpen(true);
-//           } else {
-//             alert("Some thing is wrong !!!!");
-//           }
-//         })
-//         .catch((Err) => console.error(Err));
-//     }
-//   };
+  const onDelClick = url => {
+    if (window.confirm("Are you sure to delete ?")) {
+      fetch(url, { method: "DELETE" })
+        .then((response) => {
+          if (response.ok) {
+            fetchOwners();
+            setOpen(true);
+          } else {
+            alert("Some thing is wrong !!!!");
+          }
+        })
+        .catch((Err) => console.error(Err));
+    }
+  };
   const columns = [
     { field: "firstname", headerName: "Prenom", width: 200 },
     { field: "lastname", headerName: "Nom", width: 200 },
-    // {
-    //   field: "_links.car.href",
-    //   headerName: "",
-    //   sortable: false,
-    //   filterable: false,
-    //   renderCell: row => <EditCar data={row} updateCar={updateCar}/>
-    // },
-    // {
-    //   field: "_links.self.href",
-    //   headerName: "",
-    //   sortable: false,
-    //   filterable: false,
-    //   renderCell: (row) => (
-    //     <IconButton variant="contained" color="error" size="small" onClick={() => onDelClick(row.id)}><DeleteIcon color="error"/></IconButton>
-    //   ),
-    // },
+    {
+      field: "_links.owner.href",
+      headerName: "",
+      sortable: false,
+      filterable: false,
+      renderCell: row => <EditOwner data={row} updateOwner={updateOwner}/>
+    },
+    {
+      field: "_links.self.href",
+      headerName: "",
+      sortable: false,
+      filterable: false,
+      renderCell: (row) => (
+        <IconButton variant="contained" color="error" size="small" onClick={() => onDelClick(row.id)}><DeleteIcon color="error"/></IconButton>
+      ),
+    },
   ];
 
   return (
     <React.Fragment>
-      {/* <Stack mt={2} mb={2}>
-        <AddCar addCar={addCar}/>
-      </Stack> */}
+      <Stack mt={2} mb={2}>
+        <AddOwner addOwner={addOwner}/>
+      </Stack>
       <div style={{ height: 500, width: "100%" }}>
         <DataGrid
           rows={owners}
@@ -99,12 +99,12 @@ function OwnerList() {
           columns={columns}
           getRowId={(row) => row._links.self.href}
         />
-        {/* <Snackbar
+        <Snackbar
           open={open}
           autoHideDuration={2000}
           onClose={() => setOpen(false)}
-          message="voiture suprimee"
-        /> */}
+          message="Utilisateur suprimee"
+        />
       </div>
     </React.Fragment>
   );
